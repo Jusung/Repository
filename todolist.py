e7ca41340915b0e8647a538e00e2ecfeb00425a4
계주성 <kyejusung@gmail.com>
2024-11-24 15:47:34 +0900

    show_tasks

--- todolist.py
+++ todolist.py
@@ -4,3 +4,7 @@ def add_task(task):
     todo_list.append(task)
     print(f"'{task}' added to the list!")
 
+def show_tasks():
+    print("\nTodo List:")
+    for idx, task in enumerate(todo_list, start=1):
+        print(f"{idx}. {task}")
