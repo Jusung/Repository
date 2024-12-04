b4683917d7bf03831c4a9b6c3bc05b3d903202a3
jusung <kyejusung@gmail.com>
2024-12-04 18:28:19 +0900

    added functions

--- calculator.py
+++ calculator.py
@@ -1,3 +1,12 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
 def calculator():
     print("Simple Calculator")
     num1 = float(input("Enter first number: "))
