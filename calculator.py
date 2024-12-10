5985182257447e2c23ca010753e710989de40a86
jusung Kye <kyejusung@gmail.com>
2024-12-10 14:03:35 +0900

    Update calculator.py - add little bit

--- calculator.py
+++ calculator.py
@@ -22,7 +22,7 @@ def calculator():
     elif operation == '/':
         result = num1 / num2
     else:
-        print("Invalid operation!")
+        print("Invalid operation! hello ")
         return
 
     print(f"The result is: {result} u can't")
