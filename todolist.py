47e5fad228216e513009e70368d3d2b751979e3a
jusung <kyejusung@gmail.com>
2024-12-10 14:34:56 +0900

    1,2,3,4

--- todolist.py
+++ todolist.py
@@ -17,7 +17,7 @@ def remove_task(index):
         print("Invalid index!")
         print("Invalid index! invalid.. worlds")
 
-# 실행 예제1, 2, 3
+# 실행 예제1, 2, 3, 4
 add_task("Learn Python")
 add_task("Do Git practice")
 show_tasks()
